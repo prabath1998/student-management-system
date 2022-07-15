c1df3e9fdf0a2ef6ea3bb7f5f40772802637080d
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-15 09:03:07 +0530

    get all students api added

--- src/main/java/com/test/studentsystem/controller/StudentController.java
+++ src/main/java/com/test/studentsystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.test.studentsystem.controller;
 import com.test.studentsystem.model.Student;
 import com.test.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -18,5 +17,10 @@ public class StudentController {
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
         return "New student is added";
+    }
+
+    @GetMapping("/getall")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
--- src/main/java/com/test/studentsystem/service/StudentService.java
+++ src/main/java/com/test/studentsystem/service/StudentService.java
@@ -2,6 +2,10 @@ package com.test.studentsystem.service;
 
 import com.test.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+
+    public List<Student> getAllStudents();
 }
