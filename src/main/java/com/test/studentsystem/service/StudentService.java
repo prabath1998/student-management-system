bf886c653380f535ee0759c82d523e42bc215701
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-15 08:57:01 +0530

    add student api added

--- src/main/java/com/test/studentsystem/service/StudentService.java
+++ src/main/java/com/test/studentsystem/service/StudentService.java
@@ -1,4 +1,7 @@
 package com.test.studentsystem.service;
 
+import com.test.studentsystem.model.Student;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
 }
--- src/main/java/com/test/studentsystem/service/StudentServiceImpl.java
+++ src/main/java/com/test/studentsystem/service/StudentServiceImpl.java
@@ -1,7 +1,18 @@
 package com.test.studentsystem.service;
 
+import com.test.studentsystem.model.Student;
+import com.test.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServiceImpl implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
